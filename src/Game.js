ba6aba3fcd4687a7277d3ef4a4ee3c262527e3d9
stepney141 <stepney141andi.c.i@gmail.com>
2021-06-10 17:40:06 +0900

    Defining a Game: ゲームの初期状態と着手方法を定義

---
+++ src/Game.js
@@ -0,0 +1,11 @@
+const TicTacToe = {
+  setup: () => ({ cells: Array(9).fill(null) }),
+
+  moves: {
+    clickCell: (G, ctx, id) => {
+      G.cells[id] = ctx.currentPlayer;
+    },
+  },
+};
+
+export { TicTacToe };
